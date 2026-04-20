525b24aa234eeeafd1748fe1fa8eb919cd1c4304
natasherr <ashleyotsiula@gmail.com>
2026-04-20 12:10:00 +0300

    first lesson

---
+++ learningdsa/Variables.py
@@ -0,0 +1,53 @@
+
+# Monday, 20th April 2026 (10:15AM - 12:15PM)
+print("Hello python")
+
+name = "Ashley"
+name2 = "Ashley"
+print(name)
+flag = True
+balance = 300.5462
+number = 21
+print (flag)
+print(type(flag))
+print(type(balance))
+print(type(number))
+print(type(name))
+
+#Python relies on indentation to show the scope of the code.
+# Shortcut for comments - CTRL + /
+# For multi line comments, use triple quotation marks ie. """    """"   or '''   '''
+
+'''
+Multi line comment
+Multi line comment
+'''
+
+"""
+Multi line comment
+"""
+
+print("Using for range")
+for i in range(5):
+    print(i)
+        
+print(id(name))
+print(id(name2))
+
+# name and name 2 are stored in the same address since its the same value of data. The only thing different is that the have different pointers.
+
+# print("Using for loop")
+# items = [1,2,3,4,5,6,7,8] 
+# for item in items:
+#     print(item)
+
+print(" ")
+print(" ")
+print(" ")
+
+    
+for i in range (18):
+    if i == 17:
+        print(i)
+
+
